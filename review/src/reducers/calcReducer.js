fc41d386f379a62943038c92e0698b5be78239e9
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-02-15 12:49:27 -0500

    lecture work-in-progress

--- review/src/reducers/calcReducer.js
+++ review/src/reducers/calcReducer.js
@@ -1,17 +1,22 @@
+export const initialState = {
+  currentValue: 0,
+  memory: 0
+}
+
 const reducer = (state, action) => {
-    switch (action.type) {
-      case "ADD":
-        return { ...state, currentValue: state.currentValue + action.payload };
-      case "SUBTRACT":
-        return { ...state, currentValue: state.currentValue - action.payload };
-      case "CLEAR":
-        return { ...state, currentValue: 0 };
-      case "ADD_TO_MEMORY":
-        return { ...state, memory: state.currentValue };
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case "ADD":
+      return { ...state, currentValue: state.currentValue + action.payload };
+    case "SUBTRACT":
+      return { ...state, currentValue: state.currentValue - action.payload };
+    case "CLEAR":
+      return { ...state, currentValue: 0 };
+    case "ADD_TO_MEMORY":
+      return { ...state, memory: state.currentValue };
+    default:
+      return state;
+  }
+};
   
-  export default reducer;
+export default reducer;
   
