02d50ddef93f7279ad893921e7375a452b9d4c02
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-02-15 13:10:08 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -1,40 +1,44 @@
 import React, { useReducer } from "react";
-import reducer from './reducers/calcReducer';
+import reducer, { initialState } from './reducers/calcReducer';
+
 import "./styles.css";
 
 import {addAction, subtractAction, addToMemoryAction, clearAction} from './actions/calcAction';
+
 //application
-let currentState = {
-  currentValue: 0,
-  memory: 0
-};
+// let currentState = {
+//   currentValue: 0,
+//   memory: 0
+// };
 
 
-currentState = reducer(currentState, addAction(5) );
-console.log(currentState);
+// currentState = reducer(currentState, addAction(5) );
+// console.log(currentState);
 
-currentState = reducer(currentState, subtractAction(4));
-console.log(currentState);
+// currentState = reducer(currentState, subtractAction(4));
+// console.log(currentState);
 
-currentState = reducer(currentState, subtractAction(57232));
-console.log(currentState);
+// currentState = reducer(currentState, subtractAction(57232));
+// console.log(currentState);
 
-currentState = reducer(currentState, addAction(1312312312));
-console.log(currentState);
+// currentState = reducer(currentState, addAction(1312312312));
+// console.log(currentState);
 
-currentState = reducer(currentState, addToMemoryAction());
-console.log(currentState);
+// currentState = reducer(currentState, addToMemoryAction());
+// console.log(currentState);
 
-currentState = reducer(currentState, clearAction());
-console.log(currentState);
+// currentState = reducer(currentState, clearAction());
+// console.log(currentState);
 
-currentState = reducer(currentState, addAction(5) );
-console.log(currentState);
+// currentState = reducer(currentState, addAction(5) );
+// console.log(currentState);
 
 export default function App() {
   // const [state, setState ] = useState(initialState);
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  console.log(state);
+  
   return (
     <div className="App">
       <textarea rows="1" value="0" id="total" type="text" name="ans"></textarea>
